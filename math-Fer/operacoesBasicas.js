d168ca8c4eaf4cb35d3d04b0c40c6029cc0c5918
fernandomarca <fernandomarca@hotmail.com>
2021-01-07 23:17:28 -0300

    create package test

---
+++ math-Fer/operacoesBasicas.js
@@ -0,0 +1,5 @@
+
+module.exports.op_soma = function soma(a, b) { return a + b }
+module.exports.op_mult = function mult(a, b) { return a * b }
+module.exports.op_div = function div(a, b) { return (a / b) }
+module.exports.op_sub = function sub(a, b) { return a - b }
